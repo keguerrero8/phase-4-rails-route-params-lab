a0c80fc61a9718ca35ac71de3bf08b8b19e82e23
Kevin Guerrero <guerrk83z@gmail.com>
2022-01-29 19:06:34 -0500

    commit tree b57f74299116521480b5485ecd6e0b983d6127c8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all .filter {|s| s.first_name.downcase == params[:name] || s.last_name.downcase == params[:name]}
     render json: students
+  end
+
+  def pliss
+    render json: Student.find(params[:id])
   end
 
 end
